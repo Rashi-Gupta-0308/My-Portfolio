0d92930f6c396ade9d3b74feda1aef6db08deee9
Rashi Gupta <guptarashi98266@gmail.com>
2024-01-23 08:58:17 +0530

    second commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,15 +2,15 @@ export const Bio = {
   name: "Rashi Gupta",
   roles: [
     "Full Stack Developer",
-    "MERN Stack Developer",
     "UI/UX Designer",
+    "MERN Stack Developer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Rashi-Gupta-0308",
   resume:
-    "https://drive.google.com/file/d/1GMY6qpLaUhOv1ZpUzDJO56eFrqqvnuPq/view",
+    "https://drive.google.com/file/d/1z_ApARICjwWr-MnkV2RBZFW5uM2xm9j_/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rashi-gupta-profile/",
   twitter: "https://twitter.com/rashi_gupta_18",
   insta: "https://www.instagram.com/_rashiiii17/",
@@ -76,6 +76,11 @@ export const skills = [
   {
     title: "Languages",
     skills: [
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+      },
       {
         name: "C++",
         image:
